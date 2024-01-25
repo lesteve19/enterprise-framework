3a9fe2973649f0167d6c921c3944e011a859515a
lesteve19 <lecompton19@gmail.com>
2024-01-25 14:25:32 -0500

    testing terraform provider

--- nonprod/nonprod.tf
+++ nonprod/nonprod.tf
@@ -1,7 +1,7 @@
-provider "aws" {
-    profile = "default"
-    region = "us-east-1"
-}
+# provider "aws" {
+#     profile = "default"
+#     region = "us-east-1"
+# }
 
 terraform {
     backend "s3" {
