59fc89251ce7e8262dc48b45478c5d7692747aae
lesteve19 <lecompton19@gmail.com>
2024-02-07 11:42:53 -0500

    tables population testing

--- module/table_work.py
+++ module/table_work.py
@@ -133,9 +133,11 @@ for entry in core_list:
     comp_itself = entry.split(',', 1)[0]
     core_comps.append(comp_itself)
     comp_projects = entry.split(',', 1)[1]
-    comp_projects = comp_projects.replace('"','')
-    comp_projects = comp_projects.split(',')
-    print(comp_projects)
+    for comp_project in comp_projects:
+        print(comp_project)
+    # comp_projects = comp_projects.replace('"','')
+    # comp_projects = comp_projects.split(',')
+    # print(comp_projects)
     num_projects = comp_projects.split(',')
     # print("-------------------------------------------------------")
     # print(f'{comp_itself} relies on these projects: {comp_projects}')
